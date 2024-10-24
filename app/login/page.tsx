e6764f9c31512853405f744f10679131b229a520
Th1b4uthkt <th1b4ut.hkt@gmail.com>
2024-10-24 20:22:35 +0700

    Login et action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,14 @@
-'use client'
-
-import { AuthModal } from '@/components/auth/AuthModal'
-import { useState } from 'react'
+import { login, signup } from './actions'
 
 export default function LoginPage() {
-  const [isOpen, setIsOpen] = useState(true)
-  return <AuthModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
+  )
 }
